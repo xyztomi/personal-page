ecbbec9b3786989f7f8ac45f65a39dd7cd14f572
Tomi <amandipthandi@gmail.com>
2024-09-18 23:32:59 +0700

    Refactor app.vue to use Introduction component

---
+++ components/Introduction.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="wrapper">
+    <div class="flex-center">
+    </div>
+    <p class="big-text ">♡ tomi</p>
+    <br />
+    <p>here is my only fixed address.</p>
+    <p>> i am friendly and thoughtful, and quite awfully pretty.</p>
+    <p>> i am a tech explorer willing to take on any job in the field. if it pays, i am game.</p>
+  </div>
+</template>
