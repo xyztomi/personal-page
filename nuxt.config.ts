5b5820127cb8172c9c85ca75afa585f5ca41e17a
Tomi <amandipthandi@gmail.com>
2024-09-25 05:00:24 +0700

    Switch to @nuxt/content module for simplicity

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/content'],
   css: ['@/assets/css/global.css']
 })
