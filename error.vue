1ec7345735278dd759b669b0a01d7d2890e2f705
Tomi <amandipthandi@gmail.com>
2024-09-25 07:36:41 +0700

    changed the 404 page

---
+++ error.vue
@@ -0,0 +1,6 @@
+<template #not-found>
+  <div class="flex flex-col items-center justify-center h-screen">
+    <h1 class="font-bold text-9xl">OOPS</h1>
+    <p class="font-bold text-9xl">😳</p>
+  </div>
+</template>
--- pages/posts/[slug].vue
+++ pages/posts/[slug].vue
@@ -1,13 +1,22 @@
 <template>
   <main>
-    <ContentDoc v-slot="{ doc }">
-      <article>
-        <h1 class="big-text font-bold">{{ doc.title }}</h1>
-        <ContentRenderer :value="doc" />
-      </article>
+    <ContentDoc>
+      <template v-slot="{ doc }">
+        <article>
+          <h1 class="big-text font-bold">{{ doc.title }}</h1>
+          <ContentRenderer :value="doc" />
+        </article>
+      </template>
+      <template #not-found>
+        <div class="flex flex-col items-center justify-center h-screen">
+          <h1 class="font-bold text-9xl">OOPS</h1>
+          <p class="font-bold text-9xl">😳</p>
+        </div>
+      </template>
     </ContentDoc>
   </main>
 </template>
+
 
 <script setup>
 const doc = ref(null)
