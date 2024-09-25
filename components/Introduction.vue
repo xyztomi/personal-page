40b862dad689274104ca5054e13ff4326c5c0001
Tomi <amandipthandi@gmail.com>
2024-09-26 00:46:19 +0700

    for the sake of optimization

--- components/Introduction.vue
+++ components/Introduction.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="wrapper">
-    <div class="">
-      <img src="@/assets/images/void.gif" alt="pxArt" class="w-full"/>
+    <div class="video-container">
+      <video autoplay muted loop playsinline class="w-full">
+        <source src="@/assets/images/void.webm" type="video/webm" />
+        Your browser does not support the video tag.
+      </video>
     </div>
     <br />
     <div class="divide-y divide-text">
@@ -18,3 +21,16 @@
     <p>> i am a tech explorer willing to take on any job in the field. if it pays, i am game.</p>
   </div>
 </template>
+
+<style scoped>
+.video-container {
+  width: 100%;
+  max-width: 600px;
+  margin: auto;
+}
+
+video {
+  width: 100%;
+  height: auto;
+}
+</style>
